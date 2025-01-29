36108e597c22e18ade212e1286c98d7b711056ca
Beto Rocha <roberto.blockchainresources@gmail.com>
2025-01-29 17:03:03 -0300

    one

---
+++ NearX/Python2/aula15-hands-on/main3.py
@@ -0,0 +1,18 @@
+l = [1, 2, 3, 4]
+
+
+def soma_4(x):
+    return x + 4
+
+
+def nosso_map(func, lista):
+    nova = []
+    for item in lista:
+        x = func(item)
+        nova.append(x)
+        
+    return nova
+
+result = nosso_map(soma_4, l)
+
+print(result)
---
+++ NearX/Python2/aula15-hands-on/main4.py
@@ -0,0 +1,14 @@
+class Calculadora():
+    def soma(self, x, y):
+        return x + y
+
+
+c = Calculadora()
+c.soma(5, 9)
+
+
+print()
+
+lista = [1, 2, 3, 2]
+lista.reverse()
+print(lista)
